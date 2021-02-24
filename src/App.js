78635aaf416b4901f96ff9191e31b662ae412489
Daniil7davidov <daniil7davidov@gmail.com>
2021-02-24 20:30:39 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import { Button } from '@sberdevices/ui/components/Button/Button';
 
 function App() {
     return (
         <div className="App">
-            <p>
-                <Button view="primary">Hello Plasma</Button>
-            </p>
+            <h1>HelloWorld</h1>
         </div>
     );
 }
