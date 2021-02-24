66a910970105f154f31b01776575ac347500f749
Daniil7davidov <daniil7davidov@gmail.com>
2021-02-24 21:12:45 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 
 function App() {
+    //отрисовка списка
     let numbers = [0,1,2,3,4,5];
     let listNumbers = numbers.map((number)=><li>{number}</li>)
+    listNumbers = [];
+    for (let i = 0; i<numbers.length; i++){
+        listNumbers.push(<li>{numbers[i]}</li>)
+    }
+    console.log(listNumbers);
     return (
         <div className="App">
             {listNumbers}
