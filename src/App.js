09d5c526ad2f57171e9448963e65e54f4f7a32dc
Daniil7davidov <daniil7davidov@gmail.com>
2021-02-24 20:49:49 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import React from 'react';
 function App() {
     let [index, setIndex] = React.useState(0)
 
+    function plusOne(){
+        setIndex(index+1)
+    }
+
     return (
         <div className="App">
             <h1>{index}</h1>
-            <button onClick={()=>setIndex(index+1)}>+</button>
+            <button onClick={plusOne}>+</button>
+            <button onClick={()=>setIndex(index-1)}>-</button>
+            <button onClick={()=>setIndex(0)}>0</button>
         </div>
     );
 }
