4ff67a68848f3984c983c158cf7e161005abce1b
Daniil7davidov <daniil7davidov@gmail.com>
2021-02-24 20:25:06 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Button } from '@sberdevices/ui/components/Button/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>
+                <Button view="primary">Hello Plasma</Button>
+            </p>
+        </div>
+    );
 }
 
 export default App;
